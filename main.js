84e33b0c5032098af9da4778292d738b6bdc995f
Poltree <76885489+Poltree@users.noreply.github.com>
2021-11-26 09:11:46 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 
 noseX = 0
 noseY = 0
-
+eyeX = 0
+eyeY = 0
 function setup(){
 canvas = createCanvas(600,500)
 canvas.center()
@@ -13,9 +14,11 @@ model.on("pose",gotResult);
 function draw(){
 image(webcam,0,0,600,500);
 image(clown,noseX-60,noseY+10,100,50);
+image(glasses,eyeX,eyeY,200,200);
 }
 function preload(){
     clown = loadImage("https://i.postimg.cc/xjvRR5zS/download-removebg-preview.png");
+    glasses = loadImage("https://i.postimg.cc/DyQNYs8t/spain-removebg-preview.png");
 }
 function takesnap(){
 save("Image.png");
@@ -29,5 +32,7 @@ if (results.length > 0){
     console.log(results);
     noseX=results[0].pose.nose.x;
     noseY=results[0].pose.nose.y;
+    eyeX=results[0].pose.rightEye.x;
+    eyeY=results[0].pose.rightEye.y;
 }
 }
